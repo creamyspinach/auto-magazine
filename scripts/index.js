20a7794861c47c2a79d87d2ad2c1f5369ae5d381
spinach <vaiapal12@yandex.ru>
2022-09-03 15:05:02 +0300

    added js to slider

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,12 @@ const burger = header.querySelector('.burger');
 const burgerTopLine = burger.querySelector('.burger__line_top');
 const burgerMiddleLine = burger.querySelector('.burger__line_middle');
 const burgerBottomLine = burger.querySelector('.burger__line_bottom');
-
+const gallery = document.querySelector('.gallery');
+const gallerySlider = gallery.querySelector('.gallery__slider');
+const gallerySlideCounter = gallery.querySelectorAll('img');
+const galleryBtnLeft = gallery.querySelector('.gallery__control_left');
+const galleryBtnRight = gallery.querySelector('.gallery__control_right');
+// анимация бургер-меню
 function headerBurgerChanges () {
     burgerTopLine.classList.toggle('burger__line_top_active');
     burgerMiddleLine.classList.toggle('burger__line_middle_active');
@@ -15,3 +20,23 @@ function headerBurgerChanges () {
 }
 
 burger.addEventListener('click', headerBurgerChanges);
+let slideIdx = 0;
+const changeSlide = (direction) => {
+    switch (direction) {
+        case 'right': {
+            slideIdx++;
+            if (slideIdx >= gallerySlideCounter.length) slideIdx = 0;
+            gallerySlider.style.transform = `translateX(-${slideIdx * 100}%)`;
+            break;
+        }
+        case 'left': {
+            slideIdx--;
+            if (slideIdx < 0) slideIdx = gallerySlideCounter.length - 1;
+            gallerySlider.style.transform = `translateX(-${slideIdx * 100}%)`;
+            break;
+        }
+    }
+}
+
+galleryBtnRight.addEventListener('click', () => changeSlide('right'));
+galleryBtnLeft.addEventListener('click', () => changeSlide('left'));
