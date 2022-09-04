f75a89c8569063239ae1e5c0c5062598a08c9f42
spinach <vaiapal12@yandex.ru>
2022-09-04 21:41:43 +0300

    section press done with js

--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,13 @@ const gallerySlider = gallery.querySelector('.gallery__slider');
 const gallerySlideCounter = gallery.querySelectorAll('img');
 const galleryBtnLeft = gallery.querySelector('.gallery__control_left');
 const galleryBtnRight = gallery.querySelector('.gallery__control_right');
+const press = document.querySelector('.press');
+const pressParagraph = press.querySelector('.paragraph');
+const pressParagraphSpan = press.querySelector('.paragraph__span');
+const pressLink = press.querySelector('.press__link');
+const pressFirstRadio = press.querySelector('#press__pseudo-radio-1');
+const pressSecondRadio = press.querySelector('#press__pseudo-radio-2');
+
 // анимация бургер-меню
 function headerBurgerChanges () {
     burgerTopLine.classList.toggle('burger__line_top_active');
@@ -20,6 +27,8 @@ function headerBurgerChanges () {
 }
 
 burger.addEventListener('click', headerBurgerChanges);
+
+// анимация слайдера
 let slideIdx = 0;
 const changeSlide = (direction) => {
     switch (direction) {
@@ -38,5 +47,25 @@ const changeSlide = (direction) => {
     }
 }
 
+const changePressInfo = (radio) => {
+    switch (radio) {
+        case 'first': {
+            pressParagraphSpan.textContent = `Engadget:`
+            pressParagraph.textContent = `VW’s e-BULLI concept shows how your classic van can become an EV.`;
+            pressLink.href = 'https://www.engadget.com/2020-03-20-vw-unveils-e-bulli-t1-electric-conversion.html';
+            break;
+        }
+        case 'second': {
+            pressParagraphSpan.textContent = `Drive.ru:`;
+            pressParagraph.textContent = `Вэн Volkswagen e-Bulli скрестил классику с современной техникой.`;
+            pressLink.href = 'https://www.drive.ru/news/volkswagen/5e7447bdec05c4b251000010.html';
+            break;
+        }
+    }
+}
+
 galleryBtnRight.addEventListener('click', () => changeSlide('right'));
 galleryBtnLeft.addEventListener('click', () => changeSlide('left'));
+
+pressFirstRadio.addEventListener('click', () => changePressInfo('first'));
+pressSecondRadio.addEventListener('click', () => changePressInfo('second'));
